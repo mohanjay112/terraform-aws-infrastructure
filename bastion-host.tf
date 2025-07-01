64ad6311aa45f9a4b5279508ba7cdd2142d6e508
mohanjay112 <mohanjay112@gmail.com>
2025-07-01 18:30:45 +0530

    sss

--- bastion-host.tf
+++ bastion-host.tf
@@ -1,34 +1,24 @@
 resource "aws_instance" "vprofile-bastion" {
-  ami                    = lookup(var.AMIS, var.AWS_REGION)
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.vprofilekey.key_name
-  subnet_id              = module.vpc.public_subnets[0]
+  ami                         = lookup(var.AMIS, var.AWS_REGION)
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.vprofilekey.key_name
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  count                  = var.instance_count
-  vpc_security_group_ids = [aws_security_group.vprofile-bastion-sg.id]
+  count                       = var.instance_count
+  vpc_security_group_ids      = [aws_security_group.vprofile-bastion-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install git mysql-client -y
+              git clone -b vp-rem https://github.com/devopshydclub/vprofile-project.git
+              mysql -h ${aws_db_instance.vprofile-rds.address} -u ${var.dbuser} -p${var.dbpass} accounts --ssl-mode=DISABLED < /home/ubuntu/vprofile-project/src/main/resources/db_backup.sql
+              EOF
 
   tags = {
     Name    = "vprofile-bastion"
     PROJECT = "vprofile"
   }
 
-  provisioner "file" {
-    content     = templatefile("templates/db-deploy.tmpl", { rds-endpoint = aws_db_instance.vprofile-rds.address, dbuser = var.dbuser, dbpass = var.dbpass })
-    destination = "/tmp/vprofile-dbdeploy.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/vprofile-dbdeploy.sh",
-      "sudo /tmp/vprofile-dbdeploy.sh"
-    ]
-  }
-
-  connection {
-    type        =   "ssh"
-    user        = var.USERNAME
-    private_key = file(var.PRIV_KEY_PATH)
-    host        = self.public_ip
-  }
   depends_on = [aws_db_instance.vprofile-rds]
 }
